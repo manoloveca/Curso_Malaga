aed150d2b6444990b6fb06e0c250385cb3af59be
manoloveca <manoloveca@gmail.com>
2017-10-16 16:22:11 +0200

    2

--- 01_inicio/hola-mundo2/src/app/app.component.ts
+++ 01_inicio/hola-mundo2/src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  curso = 'Angular 4.x';
+  formador = 'Alejandro Cerezo';
+  empresa = 'Icono Training Consulting';
   title = 'app';
 }
