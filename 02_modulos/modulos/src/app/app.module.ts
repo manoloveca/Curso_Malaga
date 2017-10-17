a190665aebd97a214671061822b4d9f25d960bc1
manoloveca <manoloveca@gmail.com>
2017-10-17 15:10:10 +0200

    1

--- 02_modulos/modulos/src/app/app.module.ts
+++ 02_modulos/modulos/src/app/app.module.ts
@@ -1,14 +1,14 @@
+import { SharedModule } from './shared/shared.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ 02_modulos/modulos/src/app/shared/shared.module.ts
@@ -0,0 +1,17 @@
+import { CabezaComponent } from './cabeza/cabeza.component';
+import { PieComponent } from './pie/pie.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LogoComponent } from './logo/logo.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [LogoComponent,
+    CabezaComponent,
+    PieComponent],
+  exports : [CabezaComponent,
+    PieComponent]
+})
+export class SharedModule { }
